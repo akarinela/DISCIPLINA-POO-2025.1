43877bd090f9a05ead823297f6270ac5a34baf68
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-13 11:50:56 -0300

    Update Principal.java

--- LISTA-01/EXE-PROPOSTOS/Q02/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-PROPOSTOS/Q02/src/br/edu/principal/Principal.java
@@ -1,10 +1,27 @@
 package br.edu.principal;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Double n1, n2, n3, mult;
+		
+		System.out.println("Digite o primeiro número: ");
+		n1 = sc.nextDouble();
+		
+		System.out.println("Digite o segundo número: ");
+		n2 = sc.nextDouble();
+				
+		System.out.println("Digite o terceiro numero: ");
+		n3 = sc.nextDouble();
+		
+		mult = n1 * n2 * n3;
+		
+		System.out.println("A multiplicação dos três número é: " + mult);
 
 	}
-
 }
