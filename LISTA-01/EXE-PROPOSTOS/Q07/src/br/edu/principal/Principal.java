0a1ae339ac96cdf83bea5d6dab1f78d70c02ad5b
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-13 11:55:21 -0300

    Update Principal.java

--- LISTA-01/EXE-PROPOSTOS/Q07/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-PROPOSTOS/Q07/src/br/edu/principal/Principal.java
@@ -1,10 +1,22 @@
 package br.edu.principal;
 
+import java.util.Scanner;
+
 public class Principal {
+	
+  public static void main(String[] args) {
+	  
+	  	double peso;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o seu peso: ");
+        peso = sc.nextDouble();
 
-	}
+        double pesoEngordar = peso + (peso * 15/100);
+        double pesoEmagrecer = peso - (peso * 20/100);
 
+        System.out.println("Seu peso será " + pesoEngordar + " se você engordar 15%");
+        System.out.println("Seu peso será " + pesoEmagrecer + " se você emagrecer 20%");
+		
+		}
 }
