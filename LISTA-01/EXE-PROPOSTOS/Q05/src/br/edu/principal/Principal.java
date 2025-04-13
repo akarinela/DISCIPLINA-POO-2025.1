942f05dcbbf9a1ce180e4c574664187f58cc64fe
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-13 11:54:52 -0300

    Update Principal.java

--- LISTA-01/EXE-PROPOSTOS/Q05/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-PROPOSTOS/Q05/src/br/edu/principal/Principal.java
@@ -1,10 +1,21 @@
 package br.edu.principal;
 
+import java.util.Scanner;
+
 public class Principal {
+	
+    public static void main(String[] args) {
+    	
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.print("Digite o preço do produto: ");
+        double preco = scanner.nextDouble();
 
-	}
+        double desconto = preco * 0.10;
 
+        double novoPreco = preco - desconto;
+
+        System.out.printf("Novo preço do produto com desconto de 10%%:" + novoPreco);
+
+    }
 }
