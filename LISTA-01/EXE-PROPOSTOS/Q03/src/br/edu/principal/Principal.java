9a90b3e061b4a869fe742d912e17fda7f63416a1
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-04-13 11:53:22 -0300

    Update Principal.java

--- LISTA-01/EXE-PROPOSTOS/Q03/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-PROPOSTOS/Q03/src/br/edu/principal/Principal.java
@@ -1,10 +1,24 @@
 package br.edu.principal;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+        Double n1,n2,div;
+		
+		System.out.println("Digite um número: ");
+		 n1 = sc.nextDouble();
+		 
+		System.out.println("Digite outro número: ");
+		 n2 = sc.nextDouble();
+		 
+		 div = n1 / n2;
+		 
+		System.out.println("A divisão do primeiro numero pelo segundo é: " + div);
+		
 	}
-
 }
